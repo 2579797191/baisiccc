7293f7e85727f1976d1b8b262e90394a9c509fb9
Apple <apple@177.95.152.27.broad.xm.fj.dynamic.163data.com.cn>
2016-07-27 14:39:57 +0800

    我修改了代码

--- ccc/ccc/ViewController.m
+++ ccc/ccc/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //修改了代码------
 }
 
 - (void)didReceiveMemoryWarning {
