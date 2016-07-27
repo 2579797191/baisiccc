30701db95de05991a4668d071f645a53e16e9a63
Apple <apple@177.95.152.27.broad.xm.fj.dynamic.163data.com.cn>
2016-07-27 14:36:53 +0800

    创建项目

---
+++ ccc/ccc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ccc
+//
+//  Created by Apple on 16/7/27.
+//  Copyright © 2016年 NSObject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ccc/ccc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ccc
+//
+//  Created by Apple on 16/7/27.
+//  Copyright © 2016年 NSObject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
